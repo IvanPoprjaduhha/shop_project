42ee917f76588043071ac10792542ee6ba05ddcb
Ivan Poprjaduhha <143229625+Bergwolf4880@users.noreply.github.com>
2024-09-16 22:14:09 +0200

    Add files via upload

---
+++ src/main/java/de/ait_tr/g_37_jp_shop/G37JpShopApplication.java
@@ -0,0 +1,13 @@
+package de.ait_tr.g_37_jp_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G37JpShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G37JpShopApplication.class, args);
+	}
+
+}
